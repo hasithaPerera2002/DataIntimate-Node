a78c87b70f0116f3993fbfe86796fc25eda97a63
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-06 21:30:13 +0530

    implement item repo

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,20 +1,24 @@
-import Item from "../models/itemModel.js";
+import Item from "../models/item.js";
 import asyncHandler from "../handlers/asyncErrorHandler.js";
 import CustomerError from "../handlers/customErrorHandler.js";
+import {
+  createItemRepo,
+  deleteItemRepo,
+  getAllItemsRepo,
+  getOneItemRepo,
+  updateItemRepo,
+} from "../repo/itemRepo.js";
 
 // Get all items
 const getAllItem = asyncHandler(async (req, res, next) => {
   const { page, limit } = req.query;
-  const items = await Item.find()
-    .limit(limit * 1)
-    .skip((page - 1) * limit)
-    .exec();
+  const items = await getAllItemsRepo();
   res.json(items);
 });
 
 // Get one item
 const getOneItem = asyncHandler(async (req, res, next) => {
-  const item = await Item.findById(req.params.id);
+  const item = await getOneItemRepo(req.params.id);
   if (!item) {
     return next(
       new CustomerError(`Item not found with id of ${req.params.id}`, 404)
@@ -25,22 +29,27 @@ const getOneItem = asyncHandler(async (req, res, next) => {
 
 // Create a new item
 const createItem = asyncHandler(async (req, res, next) => {
-  const item = await Item.create(req.body);
+  const item = await createItemRepo(req.body);
   res.status(201).json(item);
 });
 
 // Update a item
 const updateItem = asyncHandler(async (req, res, next) => {
-  await Item.updateOne({ _id: req.params.id }, { $set: req.body }).then(
-    (result) => {
-      res.status(200).json(result);
-    }
-  );
+  await updateItemRepo(
+    new Item(
+      req.body.name,
+      req.body.price,
+      req.body.description,
+      req.body.quantity
+    )
+  ).then((result) => {
+    res.status(200).json(result);
+  });
 });
 
 // Delete a item
 const deleteItem = asyncHandler(async (req, res, next) => {
-  await Item.deleteOne({ _id: req.params.id }).then((result) => {
+  await deleteItemRepo(req.params.id).then((result) => {
     res.status(200).json(result);
   });
 });
---
+++ repo/itemRepo.js
@@ -0,0 +1,65 @@
+import db from "../db/db";
+
+async function getAllItemsRepo() {
+  try {
+    const items = db.query("SELECT * FROM items");
+    return items.rows;
+  } catch (error) {
+    console.error("Error fetching items:", error);
+    throw error;
+  }
+}
+
+async function getOneItemRepo(id) {
+  try {
+    const item = db.query("SELECT * FROM items WHERE id = $1", [id]);
+    return item.rows[0];
+  } catch (error) {
+    console.error("Error fetching item:", error);
+    throw error;
+  }
+}
+
+async function createItemRepo(item) {
+  try {
+    const newItem = db.query(
+      "INSERT INTO items (name, price) VALUES ($1, $2) RETURNING *",
+      [item.name, item.price]
+    );
+    return newItem.rows[0];
+  } catch (error) {
+    console.error("Error creating item:", error);
+    throw error;
+  }
+}
+
+async function updateItemRepo(id, item) {
+  try {
+    const updatedItem = db.query(
+      "UPDATE items SET name=$1, price=$2 WHERE id = $3 RETURNING *",
+      [item.name, item.price, id]
+    );
+    return updatedItem.rows[0];
+  } catch (error) {
+    console.error("Error updating item:", error);
+    throw error;
+  }
+}
+
+async function deleteItemRepo(id) {
+  try {
+    const deletedItem = db.query("DELETE FROM items WHERE id = $1", [id]);
+    return deletedItem.rows[0];
+  } catch (error) {
+    console.error("Error deleting item:", error);
+    throw error;
+  }
+}
+
+export {
+  getAllItemsRepo,
+  getOneItemRepo,
+  createItemRepo,
+  updateItemRepo,
+  deleteItemRepo,
+};
