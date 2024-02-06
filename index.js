d7ce95299b6dd4efbdce4c8e8bed72ade0b8d2d0
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-06 17:29:04 +0530

    added comment for more readability

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ process.on("unhandledRejection", (err) => {
 });
 
 process.on("uncaughtException", (err) => {
+  //
   console.log(err.name, err.message);
   console.log("UNCAUGHT EXCEPTION! Shutting down...");
   process.exit(1);
